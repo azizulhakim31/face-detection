528078e33708a358564566f3822a885d3f357839
Md. Azizul Hakim <aazizul924@gmail.com>
2026-07-07 20:17:50 +0600

    final

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const video = document.getElementById('video');
 
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/models')
+]).then(startVideo)
+
 function startVideo() {
     navigator.getUserMedia(
         { video: {} },
@@ -7,5 +14,33 @@ function startVideo() {
         err => console.error(err)
     )
 }
+
+video.addEventListener('play', () => {
+    const canvas = faceapi.createCanvasFromMedia(video)
+    document.getElementById('video-container').append(canvas)
+    const displaySize = {
+        width: video.videoWidth || video.offsetWidth || video.clientWidth,
+        height: video.videoHeight || video.offsetHeight || video.clientHeight
+    }
+    if (!displaySize.width || !displaySize.height) {
+        const rect = video.getBoundingClientRect()
+        displaySize.width = rect.width
+        displaySize.height = rect.height
+    }
+    faceapi.matchDimensions(canvas, displaySize)
+    // console.log('Video is playing');
+    setInterval(async () => {
+
+        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+
+        // const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+        // console.log(detections);
+        const resizedDetections = faceapi.resizeResults(detections, displaySize)
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+        faceapi.draw.drawDetections(canvas, resizedDetections)
+        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+        faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+    }, 100)
+})
 
 // startVideo();
